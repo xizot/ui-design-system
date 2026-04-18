1faa579ab884bcd3d38252ae939204613427b853
Nguyen Van Nhat <nvnhat.17ck1@gmail.com>
2026-04-18 19:17:15 +0700

    update

--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { DEFAULT_CHECKBOX_SIZE, FORM_SIZE_STYLES, type FormSize } from '../../constants/form-sizes';
 import { Checkbox as CheckboxPrimitive } from '@base-ui/react/checkbox';
 import { CheckIcon } from 'lucide-react';
+import { DEFAULT_CHECKBOX_SIZE, FORM_SIZE_STYLES, type FormSize } from '../../constants/form-sizes';
 
 import { cn } from '../../lib/utils';
 import { FormErrorMessage } from './form-error-message';
@@ -35,7 +35,7 @@ function Checkbox({
         <CheckboxPrimitive.Root
           data-slot="checkbox"
           className={cn(
-            'rounded-xs peer relative flex shrink-0 items-center justify-center border border-input shadow-xs transition-shadow outline-none group-has-disabled/field:opacity-50 after:absolute after:-inset-x-3 after:-inset-y-2 focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 aria-invalid:aria-checked:border-primary dark:bg-input/30 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 data-checked:border-primary data-checked:bg-primary data-checked:text-primary-foreground dark:data-checked:bg-primary',
+            'rounded-xs peer relative flex shrink-0 items-center justify-center border border-input shadow-xs transition-shadow outline-none group-has-disabled/field:opacity-50 after:-inset-x-3 after:-inset-y-2 focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 aria-invalid:aria-checked:border-primary dark:bg-input/30 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40 data-checked:border-primary data-checked:bg-primary data-checked:text-primary-foreground dark:data-checked:bg-primary',
             FORM_SIZE_STYLES[size].icon,
             { 'rounded-[3px]': size === 'md' || size === 'lg' || size === 'xl' },
 
